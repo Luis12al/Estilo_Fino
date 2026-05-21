e32d34bfc14d383557dfea74ab69003d38eb4288
Luis12al <ltzmz1246@gmail.com>
2026-05-21 14:17:50 -0500

    modificaciones del frontend

--- frontend/server.js
+++ frontend/server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos de dist/
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Todas las rutas redirigen a index.html (SPA fallback)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
